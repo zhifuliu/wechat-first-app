550e92bb6c86bbf44086c28e2db014aa5f244736
zhifu <zhifu@Ocsens-MacBook-Pro.local>
2016-10-07 16:49:20 +0800

    console log

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ App({
   onLaunch: function () {
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
+    //console.log(logs)
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
   },
   getUserInfo:function(cb){
+    //console.log(cb)
+    console.log(typeof cb)
     var that = this
     if(this.globalData.userInfo){
       typeof cb == "function" && cb(this.globalData.userInfo)
--- pages/index/index.js
+++ pages/index/index.js
@@ -18,6 +18,7 @@ Page({
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
       //更新数据
+      //console.log(userInfo)
       that.setData({
         userInfo:userInfo
       })
